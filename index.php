fb25a60ffe2de321516623ace8f45db75bb5d262
Akenoa <62771658+Akenoa@users.noreply.github.com>
2020-04-04 23:02:06 +0200

    add request add
    request add does not work properly, pb on the loop, have to find why.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 class ConnexionClasse {
 
-    /* static attributes for ConnexionClasse*/
+    /* attributs static de la classe ConnexionClasse */
     /*private static $db;
     private static $hostname;
     private static $user;
@@ -10,7 +10,7 @@ class ConnexionClasse {
     private static $dsn;
     public static $dbh;
 
-    /* method to create pdo instance */
+    /*  méthode static pour créer une instance pdo */
     public static function CreerPDO(string $db,string $hostname,string $user, string $pwd): ?PDO{
         self::$dsn="mysql:dbname=$db;host=$hostname";
         try{
@@ -19,14 +19,62 @@ class ConnexionClasse {
         } 
         catch(PDOException $err){
             echo "Error : " . $err->getMessage();
+            self::$dbh=null;
+            return self::$dbh;
 
         }
         
 
     }
     
-    
+} 
+/* FIN DE LA PREMIERE CLASSE */ 
+
+class RequeteClasse{ 
+
+    public static function ReqAdd(PDO $pdoObject, Array $ParamArray){      
+        $msg="";
+        $compteur=0;
+        $stmt=null;
+        $nbElem=count($ParamArray);
+        $firstPartRequest="";
+        $secondPartRequest="";
+        $finalInsertRequest="";  
+
+        $firstPartRequest="INSERT INTO ". $ParamArray['table']." ( ";
+        //array_shift($ParamArray);
+        $arrayFieldValue=array_splice($ParamArray,1,$nbElem-1);
+        $nbElemFieldValue=count($arrayFieldValue);
+        $secondPartRequest=" VALUES (";
+        foreach ($arrayFieldValue as $key => $value) {
+            
+            $firstPartRequest.= " '$key', ";
+            $secondPartRequest.= "'$value', ";
+            $compteur=$compteur+1;
+            if($compteur===$nbElemFieldValue){ 
+                //en mettant un break on voit qu'il y a un tour de trop ds la boucle. why?!
+                //le nb element est bon et le tableau aussi.
+                $firstPartRequest.="'$key')";
+                $secondPartRequest.="'$value' )";
+            
+            }
+        } 
+        $finalInsertRequest=$firstPartRequest.$secondPartRequest;
+        $stmt=$pdoObject->prepare($finalInsertRequest);
+        $var = $stmt->execute();
+        $nbelem2=count($arrayFieldValue);
+        return $arrayFieldValue;
+        //$message="Insertion OK";
+            
+      //array_splice($tableauTrie,$i,0,$key);      
+        
+
+
+    }
+
 }
+
+/* FIN DE LA DEUXIEME CLASSE */
 
 $db="dbcourse";
 $hostname="127.0.0.1";
@@ -35,15 +83,21 @@ $pass="";
 
 $obj=ConnexionClasse::CreerPDO($db,$hostname,$usr,$pass);
 
+$tableauReqInsert=[
+    "table"=>'t_produit',
+    "nomproduit"=>"paquet de nouilles",
+    "qtepdt"=>"1",
+];
 
+$insertion=RequeteClasse::ReqAdd($obj,$tableauReqInsert);
+var_dump($insertion);
 
 
 $request="SELECT * FROM t_produit";
-
 $statement=$obj->prepare($request);
 $statement->execute(); 
-$arrayResult=$statement->fetchAll(PDO::FETCH_ASSOC); 
-print_r($arrayResult);
+$arrayResult=$statement->fetchAll(PDO::FETCH_ASSOC);  
+//print_r($arrayResult);
 ?>
 
 <!DOCTYPE html>
