ab9fb484175a842eee3acbbfe68b52221e57c7c4
Akenoa <62771658+Akenoa@users.noreply.github.com>
2020-04-05 11:34:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -32,43 +32,43 @@ class ConnexionClasse {
 
 class RequeteClasse{ 
 
-    public static function ReqAdd(PDO $pdoObject, Array $ParamArray){      
-        $msg="";
-        $compteur=0;
-        $stmt=null;
-        $nbElem=count($ParamArray);
-        $firstPartRequest="";
-        $secondPartRequest="";
-        $finalInsertRequest="";  
+    /**
+     * @param PDO $pdoObject: obj de connexion PDO pour pouvoir faire les requête
+     * @param Array $ParamArray: tableau contenant le nécessaire pour faire la requête 
+     * le tableau doit prendre la forme suivante : (1ère clé pour la table les autres fonctionnent par 
+     * paire clé-valeur pour champs et les valeurs à insérer)
+     * ['table'=>'nomtable', 'champ1'=>'value1','champ2'=>'valeur2'] 
+     * @return Bool true = insertion réussie, false : insertion échouée
+     */
+    public static function ReqAdd(PDO $pdoObject, Array $ParamArray): bool{      
+        $stmt=null; //variable qui servira a être type PDOStatement pour pouvoir execute requête dessus.
+        $firstPartRequest=""; //partie contenant l'ordre INSERT INTO avec la table et les champs
+        $secondPartRequest=""; //partie contenant la suite de l'ordre VALUE avec les valeurs à associé au champs
+        $finalInsertRequest="";  //requête finale concaténation des 2 parties de la requête
 
-        $firstPartRequest="INSERT INTO ". $ParamArray['table']." ( ";
-        //array_shift($ParamArray);
-        $arrayFieldValue=array_splice($ParamArray,1,$nbElem-1);
-        $nbElemFieldValue=count($arrayFieldValue);
+
+        $nomTable = "`".$ParamArray['table']."`"; //mettre le nom de la table entre `` syntaxe SQL
+        $firstPartRequest="INSERT INTO $nomTable ( ";
+        $arrayFieldValue=array_splice($ParamArray,1); //retirer l'élement table du tableau pour n'avoir que les champs-valeurs
+        $lastElementFieldValue=end($arrayFieldValue); //dernier élement du nouveau tableau
         $secondPartRequest=" VALUES (";
         foreach ($arrayFieldValue as $key => $value) {
-            
-            $firstPartRequest.= " '$key', ";
-            $secondPartRequest.= "'$value', ";
-            $compteur=$compteur+1;
-            if($compteur===$nbElemFieldValue){ 
-                //en mettant un break on voit qu'il y a un tour de trop ds la boucle. why?!
-                //le nb element est bon et le tableau aussi.
-                $firstPartRequest.="'$key')";
-                $secondPartRequest.="'$value' )";
-            
+            //NB: champs de la requête doivent être entre `` et les valeurs entre '' syntaxe SQL
+            if($value===$lastElementFieldValue){ 
+                //si on est à la fin du tableau on stop la chaine requete SQL
+                $firstPartRequest.="`$key`)";
+                $secondPartRequest.="'$value')";
+            } 
+            else {
+                $firstPartRequest.= " `$key`,";
+                $secondPartRequest.= "'$value',";
             }
+            
+            
         } 
         $finalInsertRequest=$firstPartRequest.$secondPartRequest;
         $stmt=$pdoObject->prepare($finalInsertRequest);
-        $var = $stmt->execute();
-        $nbelem2=count($arrayFieldValue);
-        return $arrayFieldValue;
-        //$message="Insertion OK";
-            
-      //array_splice($tableauTrie,$i,0,$key);      
-        
-
+        return $stmt->execute(); 
 
     }
 
@@ -85,8 +85,8 @@ $obj=ConnexionClasse::CreerPDO($db,$hostname,$usr,$pass);
 
 $tableauReqInsert=[
     "table"=>'t_produit',
-    "nomproduit"=>"paquet de nouilles",
-    "qtepdt"=>"1",
+    "nomproduit"=>"poisson",
+    "qtepdt"=>"3",
 ];
 
 $insertion=RequeteClasse::ReqAdd($obj,$tableauReqInsert);
